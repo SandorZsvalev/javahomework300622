82f2ab7c77fbcd75738e373e55c6c3e305d4d85c
zhvalevalex <zhvalev@gmail.com>
2022-07-07 01:43:44 +0200

    new homework

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.HashSet;
+
 public class Car {
     private int engineCapacity;
     private int engineHP;
@@ -8,38 +10,105 @@ public class Car {
     private String color;
     private String model;
 
-    public Car(int engineCapacity, int engineHP, String gear, String driveType, String color, String model) {
-        this.engineCapacity = engineCapacity;
-        this.engineHP = engineHP;
-        this.gear = gear;
-        this.driveType = driveType;
-        this.color = color;
-        this.model = model;
+    public Car(int engineCapacity, int engineHP, String driveType, String gear, String color, String model) {
+        this.engineCapacity = checkEngineCapacity (engineCapacity) ? engineCapacity : 50;
+        this.engineHP = checkEngineHP (engineHP) ? engineHP : 0;
+        this.gear = checkGear(gear) ? gear : "not specified";
+        this.driveType = checkDriveType(driveType) ? driveType : "not specified";
+        this.color = checkColor(color) ? color : "null value";
+        this.model = checkModel(model) ? model : "unknown model";
     }
 
-    public int getEngineCapacity() {
-        return engineCapacity;
+    public Car(Car car) {
+        this.engineCapacity = car.engineCapacity;
+        this.engineHP = car.engineHP;
+        this.gear = car.gear;
+        this.driveType = car.driveType;
+        this.color = car.color;
+        this.model = car.model;
+        }
+
+    public int getEngineCapacity() { return engineCapacity; }
+
+    public int getEngineHP() { return engineHP; }
+
+    public String getGear() { return gear; }
+
+    public String getDriveType() { return driveType; }
+
+    public String getColor() { return color; }
+
+    public String getModel() { return model; }
+
+    public void setEngineCapacity(int engineCapacity) {
+        this.engineCapacity = checkEngineCapacity (engineCapacity) ? engineCapacity : 50;
     }
 
-    public int getEngineHP() {
-        return engineHP;
+    public void setEngineHP(int engineHP) {
+        this.engineHP = checkEngineHP (engineHP) ? engineHP : 0;
     }
 
-    public String getGear() {
-        return gear;
+    public void setGear(String gear) {
+        this.gear = checkGear(gear) ? gear : "not specified";
     }
 
-    public String getDriveType() {
-        return driveType;
+    public void setDriveType(String driveType) {
+        this.driveType = checkDriveType(driveType) ? driveType : "not specified";
     }
 
-    public String getColor() {
-        return color;
+    public void setColor(String color) {
+        this.color = checkColor(color) ? color : "null value";
     }
 
-    public String getModel() {
-        return model;
+    public void setModel(String model) {
+        this.model = checkModel(model) ? model : "unknown model";
     }
+
+    // методы проверок данных
+    private boolean checkEngineCapacity(int engineCapacity){
+        return engineCapacity > 50;
+    }
+
+    private boolean checkEngineHP(int engineHP){
+        return engineHP > 0;
+    }
+
+    private boolean checkGear(String gear) {
+        //здесь использую регулярные выражения для проверки строкового значения
+        return gear.matches("manual|auto");
+    }
+
+    private boolean checkDriveType (String driveType) {
+        //просто чтобы потренироваться, использую здесь хэшсет
+        //не уверен, что мои знания в работе hashSet достаточно глубоки, но понимаю, что поиск нужного элемента в
+        //HashSet быстрее, чем например в ArrayList, т.к. arraylist по сути массив и поиск осуществляется
+        //перебором элементов, а в случае с HashSet у нас набор уникальных значений, и поиск происходит по ключу,
+        //который в HashSet является хэшкодом объекта.
+        //в любом случае, для поиска среди трех значений это не имеет никакой практической разницы. Можно было сделать
+        //просто if (...equals()), или сделать массив и пройти по нему циклом, или, как выше, использовать регулярные выражения
+        HashSet <String> driveTypes = new HashSet<>();
+        driveTypes.add("front wheel");
+        driveTypes.add("rear wheel");
+        driveTypes.add("four wheel");
+
+        return driveTypes.contains(driveType);
+    }
+
+    private boolean checkColor (String color) {
+        return !color.equals("");
+    }
+
+    private boolean checkModel (String model) {
+        // здесь проверяем циклом по массиву
+        String [] str = {"golf", "hatchback",  "coupe", "jeep", "compact"};
+        for (String s : str) {
+            if (model.equals(s)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     @Override
     public String toString() {
@@ -50,4 +119,29 @@ public class Car {
                 ", привод = " + driveType +
                 ", цвет = " + color;
     }
+
+    // метод меняет поля двух объектов (кроме поля "модель")
+    public static void changeCars(Car car1, Car car2){
+        int bufferEngineCap = car2.engineCapacity;
+        car2.engineCapacity=car1.engineCapacity;
+        car1.engineCapacity=bufferEngineCap;
+
+        int bufferEngineHP=car2.engineHP;
+        car2.engineHP=car1.engineHP;
+        car1.engineHP=bufferEngineHP;
+
+        String bufferGear = car2.gear;
+        car2.gear = car1.gear;
+        car1.gear = bufferGear;
+
+        String bufferDriveType = car2.driveType;
+        car2.driveType = car1.driveType;
+        car1.driveType = bufferDriveType;
+
+        String bufferColor = car2.color;
+        car2.color = car1.color;
+        car1.color = bufferColor;
+    }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,46 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
         Car car1 = new Car(1600,116,"front wheel","manual","white", "golf");
         Car car2 = new Car(3500,210,"four wheel","auto","black", "hatchback");
         Car car3 = new Car(2200,180,"rear wheel","manual","green", "coupe");
         Car car4 = new Car(5600,350,"four wheel","auto","red", "jeep");
         Car car5 = new Car(800,90,"front wheel","auto","blue","compact");
 
-        Car [] cars = {car1,car2,car3,car4,car5};
+        // сделали конструктором с некорректными значениями
+        Car car6 = new Car(0,0,"aaa","aaa","","zzz");
+        // сеттерами задаем корректные значения
+        car6.setEngineCapacity(1400);
+        car6.setEngineHP(110);
+        car6.setDriveType("front wheel");
+        car6.setGear("manual");
+        car6.setColor("white");
+        car6.setModel("hatchback");
 
-        for (Car car : cars) {
-            System.out.println(car);
-        }
+        // делаем дубликаты с помощью конструктора копирования
+        Car car7 = new Car (car1);
+        Car car8 = new Car (car2);
 
-    // но если менее очевидным путем, то так:
-/*
+        Car [] cars = {car1,car2,car3,car4,car5,car6,car7,car8};
+
+        // вывод с помощью переопределенного метода toString
+//        for (Car car : cars) {
+//            System.out.println(car);
+//       }
+
+        // вывод с помощью геттеров через массив c помощью отдельного метода
+        printCars(cars);
+
+        System.out.println("___________________");
+
+        // заменяем первую машину на вторую, оставляем не тронутым только первый параметр - модель
+        Car.changeCars (car1,car2);
+
+        printCars(cars);
+
+    }
+
+    public static void printCars (Car [] cars){
         for (Car car : cars) {
             System.out.println( "Автомобиль "+car.getModel() +
                     ", объем двигателя "+car.getEngineCapacity() +
@@ -27,7 +52,6 @@ public class Main {
                     ", цвет "+car.getColor()
             );
         }
-*/
-
     }
+
 }
